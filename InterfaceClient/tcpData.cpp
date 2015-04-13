bfa51be7b306099c5f650fc101d42ba1a2255e10
juniperdiego <279588288@163.com>
2015-04-13 22:41:59 +0800

    add tcpData.cpp and tcpData.h instead of original tcpDataGen.h and tcpDataGen.cpp

---
+++ InterfaceClient/tcpData.cpp
@@ -0,0 +1,263 @@
+#include "tcpData.h"
+
+char degree2Binary(float degree)
+{
+	return degree/5.625f;
+	
+}
+
+tcpData::tcpData(char dataHeader, char cmd, char dataLen)
+{
+	m_dataHeader = dataHeader;
+	m_cmd = cmd;
+	m_dataLen = dataLen;
+	m_dataLenOccupyBytes = 1;//default, it is ususal except compTCorrection
+}
+
+tcpData::~tcpData()
+{
+}
+
+int tcpData::getDataSize()
+{
+	return m_dataLen + 3 + m_dataLenOccupyBytes;
+}
+
+compTCorrection::compTCorrection()
+	:tcpData(0x92, 0xCC, 0x200)
+{
+	m_dataLenOccupyBytes = 2;//compTCorrection
+}
+
+void compTCorrection::setDegree(vector<vector<float> > degreeMatrix)
+{
+	m_degreeMatrix = degreeMatrix;
+}
+
+void compTCorrection::setEnable(vector<vector<bool> > enableMatrix)
+{
+	m_enableMatrix = enableMatrix;
+}
+
+vector<int> compTCorrection::generate()
+{
+	vector<int> res;
+
+	if(m_degreeMatrix.size() == 0 || m_enableMatrix.size() == 0)
+		return res;
+
+	res.push_back(m_dataHeader);
+	res.push_back(m_cmd);
+	res.push_back((m_dataLen >> 8) & 0xff);
+	res.push_back(m_dataLen & 0xff);
+
+	int rowSize = m_degreeMatrix.size();
+	int colSize = m_degreeMatrix[0].size();
+
+
+	for(int i = 0;  i < rowSize; i++)
+	{
+		for(int j = 0; j < colSize; j++)
+		{
+			int data = 0;
+			data = (i+1) >> 12 |  	// x axis
+				(j+1) >> 8 |	// y axis
+				m_enableMatrix[i][j] >>7 |// enable
+				degree2Binary(m_degreeMatrix[i][j]) >> 1 | //degree
+				0x1;
+
+			// data occupy two bytes, so first push its high byte, then low byte
+			res.push_back((data >> 8) & 0xff);
+			res.push_back(data & 0xff);
+		}
+	}
+
+	// calc the check
+	char check = 0;
+	for(int i = 2 + m_dataLenOccupyBytes; i < res.size(); i++)
+	{
+		check = check ^ (res[i] & 0xff);
+	}
+	res.push_back(check);
+	return res;
+
+}
+
+compRCorrection::compRCorrection()
+	:tcpData(0x92, 0xCB, 0x6C)
+{
+}
+
+void compRCorrection::setDegree(vector<vector<float> > degreeMatrix)
+{
+	m_degreeMatrix = degreeMatrix;
+}
+
+void compRCorrection::setEnable(vector<vector<bool> > enableMatrix)
+{
+	m_enableMatrix = enableMatrix;
+}
+
+vector<int> compRCorrection::generate()
+{
+	vector<int> res;
+
+	if(m_degreeMatrix.size() == 0 || m_enableMatrix.size() == 0)
+		return res;
+
+	res.push_back(m_dataHeader);
+	res.push_back(m_cmd);
+	res.push_back(m_dataLen & 0xff);
+
+	int rowSize = m_degreeMatrix.size();
+	int colSize = m_degreeMatrix[0].size();
+
+
+	for(int i = 0;  i < rowSize; i++)
+	{
+		for(int j = 0; j < colSize; j++)
+		{
+			int data = 0;
+			data = (i+1) >> 20 |  	// x axis
+				(j+1) >> 16 |	// y axis
+			//??	m_enableMatrix[i][j] >>7 |// enable
+				degree2Binary(m_degreeMatrix[i][j]) >> 0 | //degree
+				0x1;
+
+			// data occupy 3 bytes, 
+			res.push_back((data >> 16) & 0xff);
+			res.push_back((data >> 8) & 0xff);
+			res.push_back(data & 0xff);
+		}
+	}
+
+	// calc the check
+	char check = 0;
+	for(int i = 2 + m_dataLenOccupyBytes; i < res.size(); i++)
+	{
+		check = check ^ (res[i] & 0xff);
+	}
+	res.push_back(check);
+	return res;
+
+}
+
+
+compTTestCode::compTTestCode()
+	:tcpData(0x92, 0x5A, 0x20)
+{
+}
+
+void compTTestCode::setEnable(vector<vector<bool> > enableMatrix)
+{
+	m_enableMatrix = enableMatrix;
+}
+
+vector<int> compTTestCode::generate()
+{
+	vector<int> res;
+
+	if(m_enableMatrix.size() == 0)
+		return res;
+
+	int rowSize = m_enableMatrix.size();
+	int colSize = m_enableMatrix[0].size();
+
+	if(rowSize != 16 || colSize != 16)
+		return res;
+
+	res.push_back(m_dataHeader);
+	res.push_back(m_cmd);
+	res.push_back(m_dataLen & 0xff);
+
+	int data = 0;
+	for(int i = 0;  i < rowSize; i++)
+	{
+		data = 0;
+		for(int j = 0; j < colSize/2; j++)
+		{
+			if(m_enableMatrix[i][j])
+				data = data | (1 << j);
+		}
+		res.push_back(data);
+
+		data = 0;
+		for(int j = colSize/2 + 1; j < colSize; j++)
+		{
+			if(m_enableMatrix[i][j])
+				data = data | (1 << (j- colSize/2));
+		}
+		res.push_back(data);
+	}
+
+	// calc the check
+	char check = 0;
+	for(int i = 2 + m_dataLenOccupyBytes; i < res.size(); i++)
+	{
+		check = check ^ (res[i] & 0xff);
+	}
+	res.push_back(check);
+	return res;
+
+}
+
+
+compRTestCode::compRTestCode()
+	:tcpData(0x92, 0xA5, 0x05)
+{
+}
+
+void compRTestCode::setEnable(vector<vector<bool> > enableMatrix)
+{
+	m_enableMatrix = enableMatrix;
+}
+
+vector<int> compRTestCode::generate()
+{
+	vector<int> res;
+
+	if(m_enableMatrix.size() == 0)
+		return res;
+
+	int rowSize = m_enableMatrix.size();
+	int colSize = m_enableMatrix[0].size();
+
+	if(rowSize != 6 || colSize != 6)
+		return res;
+
+	vector<bool> enableVec;
+
+	for(int i = 0; i < rowSize; i++)
+	{
+		for(int j = 0; j < rowSize; j++)
+		{
+			enableVec.push_back(m_enableMatrix[i][j]);
+		}
+	}
+
+	res.push_back(m_dataHeader);
+	res.push_back(m_cmd);
+	res.push_back(m_dataLen & 0xff);
+
+	int data = 0;
+	for(int i = 0;  i < enableVec.size(); i++)
+	{
+		if(enableVec[i])
+			data = data | (1 << (i % 8));
+		if( (i+1) % 8 == 0 || (i + 1) == enableVec.size() )
+		{
+			res.push_back(data);
+			data = 0;
+		}
+	}
+
+	// calc the check
+	char check = 0;
+	for(int i = 2 + m_dataLenOccupyBytes; i < res.size(); i++)
+	{
+		check = check ^ (res[i] & 0xff);
+	}
+	res.push_back(check);
+	return res;
+
+}
---
+++ InterfaceClient/tcpData.h
@@ -0,0 +1,65 @@
+#ifndef TCP_DATA_H
+#define TCP_DATA_H
+
+#include <vector>
+using std::vector;
+
+class tcpData
+{
+public:
+	tcpData(char dataHeader, char cmd, char dataLen);
+	~tcpData();
+	virtual vector<int>  generate() =0;
+	int getDataSize();
+protected:
+	char	m_dataHeader;
+	char	m_cmd;
+	unsigned int	m_dataLen;
+	unsigned int	m_dataLenOccupyBytes;
+	char	m_check;
+};
+
+class compTCorrection : public tcpData
+{
+public:
+	compTCorrection();
+	vector<int> generate();
+	void setDegree(vector<vector<float> > degreeMatrix);
+	void setEnable(vector<vector<bool> > enableMatrix);
+private:
+	vector<vector<float> > m_degreeMatrix;
+	vector<vector<bool> > m_enableMatrix;
+};
+
+class compRCorrection : public tcpData
+{
+public:
+	compRCorrection();
+	vector<int> generate();
+	void setDegree(vector<vector<float> > degreeMatrix);
+	void setEnable(vector<vector<bool> > enableMatrix);
+private:
+	vector<vector<float> > m_degreeMatrix;
+	vector<vector<bool> > m_enableMatrix;
+};
+
+class compTTestCode: public tcpData
+{
+public:
+	compTTestCode();
+	vector<int> generate();
+	void setEnable(vector<vector<bool> > enableMatrix);
+private:
+	vector<vector<bool> > m_enableMatrix;
+};
+
+class compRTestCode: public tcpData
+{
+public:
+	compRTestCode();
+	vector<int> generate();
+	void setEnable(vector<vector<bool> > enableMatrix);
+private:
+	vector<vector<bool> > m_enableMatrix;
+};
+#endif // TCP_DATA_GEN
